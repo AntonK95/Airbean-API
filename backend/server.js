e88d42bfb9a04b2d7feb9c65df537219a9a60efb
divperedi <132815746+divperedi@users.noreply.github.com>
2024-05-29 14:24:03 +0200

    post register done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import menuRouter from './routes/menu.js';
+import nedb from 'nedb-promises';
+import registerRouter from './routes/register.js';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+export const database = nedb.create({ filename: 'users.db', autoload: true });
+
 app.use(express.json());
 app.use('/menu', menuRouter);
+app.use('/register', registerRouter);
 
 // Starta server
 app.listen(PORT, (req, res) => {
